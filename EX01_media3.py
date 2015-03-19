373ca6a65ac0c1f93cee85dd85038d623ef89cf5
LuizTiberio <luiztiberio3@yahoo.com.br>
2015-03-19 18:56:24 -0300

    Aula 2
    Atividades relativas à aula 2: Função é Par e função média,
    implementadas em Python 3.0

---
+++ EX01_media3.py
@@ -0,0 +1,10 @@
+def media(num1, num2, num3):
+    resposta = (num1+num2+num3)/3
+    return resposta
+
+a=float(input('digite a nota 1:  '))
+b=float(input('agora a nota 2:  '))
+c=float(input('por fim a nota 3:  '))
+print(media(a,b,c))
+
+
---
+++ EX02_par.py
@@ -0,0 +1,8 @@
+def par(numero):
+    if(numero % 2 ==0):
+      return 'par'
+    else:
+      return 'impar'
+
+numero= int(input('digite o numero:  '))
+print(par(numero))
